cf594d2d7a8bccdce3af8f10653de342e8a06841
Justin Meunier <Justin Meunier>
2017-04-28 17:42:21 -0500

    Added Shiny rough draft

--- 02 Shiny/server.R
+++ 02 Shiny/server.R
@@ -11,7 +11,7 @@ require(DT)
 dfs <- data.frame(query(
   data.world(propsfile = "www/.data.world"),
   dataset="vcjaladi/s-17-dv-final-project", type="sql",
-  query="SELECT CountyElections.State, sum(CountyElections.`Total.Population`), sum(CountyElections.votes), sum(CountyElections.votes16_trumpd), sum(CountyElections.votes16_clintonh), sum(CountyElections.votes16_johnsong), sum(CountyElections.votes16_steinj),avg(CountyElections.rep16_frac) as RepPCT, avg(CountyElections.dem16_frac) as DemPCT, avg(CountyElections.`At.Least.Bachelor.s.Degree`), avg(CountyElections.`At.Least.High.School.Diploma`), avg(CountyElections.`Less.Than.High.School`),avg(CountyElections.`Graduate.Degree`), avg(CountyElections.`White.not.Latino.Population`), avg(CountyElections.`African.American.Population`),avg(CountyElections.`Native.American.Population`), avg(CountyElections.`Asian.American.Population`),avg(CountyElections.`Population.some.other.race.or.races`), avg(CountyElections.`Latino.Population`),avg(CountyElections.`Management.professional.and.related.occupations`), avg(CountyElections.`Service.occupations`),avg(CountyElections.`Sales.and.office.occupations`), avg(CountyElections.`Farming.fishing.and.forestry.occupations`),avg(CountyElections.`Construction.extraction.maintenance.and.repair.occupations`), avg(CountyElections.`Production.transportation.and.material.moving.occupations`), avg(CountyElections.`Adult.obesity`), avg(CountyElections.Diabetes), avg(CountyElections.Uninsured), avg(CountyElections.Unemployment), 
+  query="SELECT CountyElections.State, CountyElections.Region, sum(CountyElections.`Total.Population`), sum(CountyElections.votes), sum(CountyElections.votes16_trumpd), sum(CountyElections.votes16_clintonh), sum(CountyElections.votes16_johnsong), sum(CountyElections.votes16_steinj),avg(CountyElections.rep16_frac) as RepPCT, avg(CountyElections.dem16_frac) as DemPCT, avg(CountyElections.`At.Least.Bachelor.s.Degree`), avg(CountyElections.`At.Least.High.School.Diploma`), avg(CountyElections.`Less.Than.High.School`),avg(CountyElections.`Graduate.Degree`), avg(CountyElections.`White.not.Latino.Population`), avg(CountyElections.`African.American.Population`),avg(CountyElections.`Native.American.Population`), avg(CountyElections.`Asian.American.Population`),avg(CountyElections.`Population.some.other.race.or.races`), avg(CountyElections.`Latino.Population`),avg(CountyElections.`Management.professional.and.related.occupations`), avg(CountyElections.`Service.occupations`),avg(CountyElections.`Sales.and.office.occupations`), avg(CountyElections.`Farming.fishing.and.forestry.occupations`),avg(CountyElections.`Construction.extraction.maintenance.and.repair.occupations`), avg(CountyElections.`Production.transportation.and.material.moving.occupations`), avg(CountyElections.`Adult.obesity`), avg(CountyElections.Diabetes), avg(CountyElections.Uninsured), avg(CountyElections.Unemployment), 
 Income.AreaName, sum(Income.B19013_001) / 1000 as median_income_thousands, 
   Poverty.AreaName, sum(Poverty.B17001_002) / 1000 as numinpov_thousands, 
   Race.AreaName, sum(Race.B02001_002) / 1000 as sum_white_thousands
@@ -25,17 +25,147 @@ Income.AreaName, sum(Income.B19013_001) / 1000 as median_income_thousands,
   ORDER BY CountyElections.State"
 ))
 
-View(dfs)
+#View(dfs)
+
+region_list <- unique(dfs$Region)
+region_list <- append(list("All" = "All"), region_list)
+
 
 shinyServer(function(input, output) { 
   # These widgets are for the Crosstabs tab.
   TVM <- reactive({input$TVM1})
   HVM <- reactive({input$HVM1})
+
+# Begin Box Plot Tab ------------------------------------------------------------------
+  
+  # Parameterization 
+  df_bp <- eventReactive(input$click1, {
+    
+    dfs %>% dplyr::mutate(
+      victory_margin = if_else(RepPCT >0.5,
+                               (if_else(RepPCT > TVM(), 'Trump Landslide Victory',
+                                        'Trump Victory')
+                               ),
+                               if_else(DemPCT > HVM(),
+                                       'Hillary Landslide Victory',
+                                       'Hillary Victory')))
+  })
+  
+  # # output the plot
+  output$plot1 <- renderPlot({ggplot(df_bp()) +
+      theme(axis.text.x=element_text(size=16, vjust=0.5)) +
+      theme(axis.text.y=element_text(size=16, hjust=0.5)) +
+      geom_boxplot(aes(x=AreaName, y=median_income_thousands, fill=victory_margin)) +
+      geom_text(aes(x=AreaName, y=median_income_thousands, label=median_income_thousands, hjust=-0.25)) +
+      labs(title="Box Plot Example", y="Medium Income (Thousands USD)", x="State") +
+      coord_flip()
+    
+  })
+  
+  
+  # End Boxplot Tab ___________________________________________________________  
+  
+  
+# Begin Histogram Tab ------------------------------------------------------------------
+  
+  # Parameterization 
+  df_hg <- eventReactive(input$click2, {
+    
+    dfs %>% dplyr::mutate(
+      victory_margin = if_else(RepPCT >0.5,
+                               (if_else(RepPCT > TVM(), 'Trump Landslide Victory',
+                                        'Trump Victory')
+                               ),
+                               if_else(DemPCT > HVM(),
+                                       'Hillary Landslide Victory',
+                                       'Hillary Victory')))
+  })
+  
+  # # output the plot
+  output$plot2 <- renderPlot({ggplot(df_hg()) +
+      theme(axis.text.x=element_text(size=16, vjust=0.5)) +
+      theme(axis.text.y=element_text(size=16, hjust=0.5)) +
+      geom_histogram(aes(x=AreaName, y=median_income_thousands, fill=victory_margin)) +
+      geom_text(aes(x=AreaName, y=median_income_thousands, label=median_income_thousands, hjust=-0.25)) +
+      labs(title="Histogram Example", y="Medium Income (Thousands USD)", x="State") +
+      coord_flip()
+    
+  })
+  
+  
+  # End Histogram Tab ___________________________________________________________   
+  
+  
+  
+  
+# Begin Scatterplot Tab ------------------------------------------------------------------
+  
+  # Parameterization 
+  df_sp <- eventReactive(input$click3, {
+    
+    dfs %>% dplyr::mutate(
+      victory_margin = if_else(RepPCT >0.5,
+                               (if_else(RepPCT > TVM(), 'Trump Landslide Victory',
+                                        'Trump Victory')
+                               ),
+                               if_else(DemPCT > HVM(),
+                                       'Hillary Landslide Victory',
+                                       'Hillary Victory')))
+  })
+  
+  # # output the plot
+  output$plot3 <- renderPlot({ggplot(df_sp()) +
+      theme(axis.text.x=element_text(size=16, vjust=0.5)) +
+      theme(axis.text.y=element_text(size=16, hjust=0.5)) +
+      geom_point(aes(x=AreaName, y=median_income_thousands, fill=victory_margin)) +
+      geom_text(aes(x=AreaName, y=median_income_thousands, label=median_income_thousands, hjust=-0.25)) +
+      labs(title="Scatter Plot Example", y="Medium Income (Thousands USD)", x="State") +
+      coord_flip()
+    
+  })
+  
+  
+# End Scatterplot Tab ___________________________________________________________   
+  
+  
+
   
 # Begin Crosstab Tab ------------------------------------------------------------------
   
   # Parameterization 
-  df_full <- eventReactive(input$click1, {
+  df_ct <- eventReactive(input$click4, {
+    
+    dfs %>% dplyr::mutate(
+      victory_margin = if_else(RepPCT >0.5,
+                               (if_else(RepPCT > TVM(), 'Trump Landslide Victory',
+                                        'Trump Victory')
+                               ),
+                               if_else(DemPCT > HVM(),
+                                       'Hillary Landslide Victory',
+                                       'Hillary Victory')))
+  })
+  
+  # # output the plot
+  output$plot4 <- renderPlot({ggplot(df_ct()) +
+      theme(axis.text.x=element_text(size=16, vjust=0.5)) +
+      theme(axis.text.y=element_text(size=16, hjust=0.5)) +
+      geom_tile(aes(x=Region, y=median_income_thousands, fill=victory_margin)) +
+      geom_text(aes(x=Region, y=median_income_thousands, label=median_income_thousands, hjust=-0.25)) +
+      labs(title="Crosstab Example", y="Medium Income (Thousands USD)", x="State") +
+      coord_flip()
+    
+  })
+  
+  
+# End Crosstab Tab ___________________________________________________________ 
+  
+  
+  
+  
+# Begin Barchart Tab ------------------------------------------------------------------
+  
+  # Parameterization 
+  df_bc <- eventReactive(input$click5, {
   
   dfs %>% dplyr::mutate(
     victory_margin = if_else(RepPCT >0.5,
@@ -47,32 +177,20 @@ shinyServer(function(input, output) {
                                       'Hillary Victory')))
   })
   
-  # # output the plots
-  output$plot1 <- renderPlot({ggplot(df_full()) +
+  # # output the plot
+  output$plot5 <- renderPlot({ggplot(df_bc()) +
     theme(axis.text.x=element_text(size=16, vjust=0.5)) +
     theme(axis.text.y=element_text(size=16, hjust=0.5)) +
-    geom_col(aes(x=AreaName, y=median_income_thousands, fill=victory_margin)) +
-    geom_text(aes(x=AreaName, y=median_income_thousands, label=median_income_thousands, hjust=-0.25)) +
-    labs(title="Median Income for Each State by Election Victory Margin", y="Medium Income (Thousands USD)", x="State") +
+    geom_col(aes(x=Region, y=median_income_thousands, fill=victory_margin)) +
+    geom_text(aes(x=Region, y=median_income_thousands, label=median_income_thousands, hjust=-0.25)) +
+    labs(title="Bar Chart Example", y="Medium Income (Thousands USD)", x="State") +
     coord_flip()
 
-  })
-  output$plot2 <- renderPlot({ggplot(df_full()) +
-      theme(axis.text.x=element_text(size=16, vjust=0.5)) +
-      theme(axis.text.y=element_text(size=16, hjust=0.5)) +
-      geom_col(aes(x=AreaName, y=numinpov_thousands, fill=victory_margin)) +
-      geom_text(aes(x=AreaName, y=numinpov_thousands, label=numinpov_thousands, hjust=-0.25)) +
-      labs(title="Population Below Poverty Line for Each State by Election Victory Margin", y="Citizens in Poverty (Thousands)", x="State") +
-      coord_flip()
-  })
-  output$plot3 <- renderPlot({ggplot(df_full()) +
-      theme(axis.text.x=element_text(size=16, vjust=0.5)) +
-      theme(axis.text.y=element_text(size=16, hjust=0.5)) +
-      geom_col(aes(x=AreaName, y=sum_white_thousands, fill=victory_margin)) +
-      geom_text(aes(x=AreaName, y=sum_white_thousands, label=sum_white_thousands, hjust=-0.25)) +
-      labs(title="Population of Whites for Each State by Election Victory Margin", y="Citizens Identifying as White (Thousands)", x="State") +
-      coord_flip()
-
-  })
 })
-# End Crosstab Tab ___________________________________________________________
+  
+
+# End Barchart Tab ___________________________________________________________
+  
+
+# End ShinyServer
+})
--- 02 Shiny/ui.R
+++ 02 Shiny/ui.R
@@ -7,29 +7,91 @@ dashboardPage(
   ),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Election Bar Charts", tabName = "BarCharts", icon = icon("dashboard"))
+      menuItem("Box Plots", tabName = "boxplot", icon = icon("dashboard")),
+      menuItem("Histograms", tabName = "histogram", icon = icon("dashboard")),
+      menuItem("Scatter Plots", tabName = "scatter", icon = icon("dashboard")),
+      menuItem("Crosstabs", tabName = "crosstab", icon = icon("dashboard")),
+      menuItem("Election Bar Charts", tabName = "barchart", icon = icon("dashboard"))
     )
   ),
   dashboardBody(    
     tabItems(
+      # Begin Box Plot content.
+      tabItem(tabName = "boxplot",
+              tabsetPanel(
+                tabPanel("Data",  
+                         sliderInput("TVM1", "Select Cutoff for Trump Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         sliderInput("HVM1", "Select Cutoff for Hillary Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         actionButton(inputId= "click1", label ="Generate Box Plot"),
+                         hr(), # Add space after button.
+                         plotOutput("plot1", height=1000)
+                ))
+      ),
+      # End Histogram tab content.
+      
+      # Begin Histogram content.
+      tabItem(tabName = "histogram",
+              tabsetPanel(
+                tabPanel("Data",  
+                         sliderInput("TVM1", "Select Cutoff for Trump Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         sliderInput("HVM1", "Select Cutoff for Hillary Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         actionButton(inputId= "click2", label ="Generate Histogram"),
+                         hr(), # Add space after button.
+                         plotOutput("plot2", height=1000)
+                ))
+      ),
+      # End Histogram tab content.
+      
+      # Begin Scatterplot content.
+      tabItem(tabName = "scatter",
+              tabsetPanel(
+                tabPanel("Data",  
+                         sliderInput("TVM1", "Select Cutoff for Trump Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         sliderInput("HVM1", "Select Cutoff for Hillary Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         actionButton(inputId= "click3", label ="Generate Scatter Plot"),
+                         hr(), # Add space after button.
+                         plotOutput("plot3", height=1000)
+                ))
+      ),
+      # End Scatterplot tab content.
+      
+      # Begin Crosstabs content.
+      tabItem(tabName = "crosstab",
+              tabsetPanel(
+                tabPanel("Data",  
+                         sliderInput("TVM1", "Select Cutoff for Trump Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         sliderInput("HVM1", "Select Cutoff for Hillary Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         actionButton(inputId= "click4", label ="Generate Crosstab"),
+                         hr(), # Add space after button.
+                         plotOutput("plot4", height=1000)
+                ))
+      ),
+      # End Crosstabs tab content.
+      
+      
+      
       # Begin Bar Charts tab content.
-      tabItem(tabName = "BarCharts",
-        tabsetPanel(
-            tabPanel("Data",  
-              sliderInput("TVM1", "Select Cutoff for Trump Landslide Victory:", 
-                          min = .5, max = 1,  value = .75),
-              sliderInput("HVM1", "Select Cutoff for Hillary Landslide Victory:", 
-                          min = .5, max = 1,  value = .75),
-              actionButton(inputId= "click1", label ="Generate Bar Charts"),
-              hr(), # Add space after button.
-              plotOutput("plot1", height=1000),
-              hr(),
-              plotOutput("plot2", height=1000),
-              hr(),
-              plotOutput("plot3", height=1000)
-            ))
-        )
-      # End Crosstab tab content.
+      tabItem(tabName = "barchart",
+              tabsetPanel(
+                tabPanel("Data",  
+                         sliderInput("TVM1", "Select Cutoff for Trump Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         sliderInput("HVM1", "Select Cutoff for Hillary Landslide Victory:", 
+                                     min = .5, max = 1,  value = .75),
+                         actionButton(inputId= "click5", label ="Generate Bar Chart"),
+                         hr(), # Add space after button.
+                         plotOutput("plot5", height=1000)
+                ))
+      )
+      # End Bar Charts tab content.
     )
   )
 )
